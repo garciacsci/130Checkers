6912c031666b30cb88b8c5dccab27c9ea741e8f5
garciacce23 <62458331+garciacce23@users.noreply.github.com>
2023-12-01 18:46:29 -0800

    Made cookies.js more robust

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,6 +1,31 @@
-function getCookie(name) {
-    let value = "; " + document.cookie;
-    let parts = value.split("; " + name + "=");
-    if (parts.length === 2) return parts.pop().split(";").shift();
-}
-let userId = getCookie("user_id");
+const Cookies = {
+    // Sets a cookie
+    set: function(name, value, hours) {
+        let expires = "";
+        if (hours) {
+            const date = new Date();
+            date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    },
+
+    // Gets a cookie by name
+    get: function(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    },
+
+    // Deletes a cookie by name
+    delete: function(name) {
+        document.cookie = name + '=; Max-Age=-99999999;';
+    }
+};
+
+export default Cookies;
