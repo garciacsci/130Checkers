79bb240c78a56796eecc96c4bcdcd197b712a693
garciacce23 <62458331+garciacce23@users.noreply.github.com>
2023-12-05 15:56:49 -0800

    Updated gameAPI.php to keep in line with updated games table schema

--- server/gameAPI.php
+++ server/gameAPI.php
@@ -4,19 +4,26 @@ include 'dbConnect.php'; // Connect to database
 
 // /endgame
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_GET['action'] == 'endGame') {
-    $game_id = filter_input(INPUT_POST, 'game_id', FILTER_SANITIZE_NUMBER_INT);
     $player_id = filter_input(INPUT_POST, 'player_id', FILTER_SANITIZE_NUMBER_INT);
     $score = filter_input(INPUT_POST, 'score', FILTER_SANITIZE_NUMBER_INT);
     $duration = filter_input(INPUT_POST, 'duration', FILTER_UNSAFE_RAW);
     $turns = filter_input(INPUT_POST, 'turns', FILTER_SANITIZE_NUMBER_INT);
+    $won = filter_input(INPUT_POST, 'won', FILTER_SANITIZE_NUMBER_INT); // Assuming 'won' is sent as an integer (1 for true, 0 for false)
 
     try {
-        $sql = "UPDATE Games SET player_id = :player_id, score = :score, duration = :duration, turns = :turns WHERE game_id = :game_id";
+        $sql = "INSERT INTO Games (player_id, score, duration, turns, won) VALUES (:player_id, :score, :duration, :turns, :won)";
         $stmt = $conn->prepare($sql);
-        $success = $stmt->execute(['score' => $score, 'duration' => $duration, 'turns' => $turns, 'game_id' => $game_id]);
+        $success = $stmt->execute([
+            'player_id' => $player_id,
+            'score' => $score,
+            'duration' => $duration,
+            'turns' => $turns,
+            'won' => $won
+        ]);
 
         if ($success) {
-            echo json_encode(["message" => "Game results recorded successfully for game ID: $game_id"]);
+            $game_id = $conn->lastInsertId();
+            echo json_encode(["message" => "Game results recorded successfully", "game_id" => $game_id]);
         } else {
             echo json_encode(["error" => "Failed to record game results"]);
         }
